7c6c108b3e4e5eb7fff74fa98383ab0927da786e
Eric Borts <eborts@bltek.com>
2012-10-03 21:06:50 -0600

    Added HTTP Image request and command line app
    Command line app doesn't yet take arguments
    but shows that it is possible to use HTTP POST
    to request the YUML diagram.

---
+++ YumlApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ToYuml;
+
+namespace YumlApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			YumlRequest.Request("[A]->[B]", true, "a_b.png");
+			YumlRequest.Request("[A]->[B],[A]^-[C]", false, "abc.png");
+		}
+	}
+}
